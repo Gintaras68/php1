e783c4cf9827d2f55767ac7ff768502d7a12e93b
gintaras68 <gintaras5783@yahoo.com>
2023-12-12 16:57:20 +0200

    pataisyta peradresavimo salyga

--- rose.php
+++ rose.php
@@ -3,8 +3,10 @@
   // * << Į pink.php puslapį įdėkite formą su POST metodu ir mygtuku “GO TO ROSE”. 
   // *    Formą nukreipkite, kad ji atsidarinėtų rose.php puslapyje. 
   // !  <<  Padarykite taip, kad surinkus naršyklėje tiesiogiai adresą į rose.php puslapį, naršyklė būtų peradresuojama į pink.php puslapį. 
-
-  header("Location: ./pink.php");
+  
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    header("Location: ./pink.php");
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
