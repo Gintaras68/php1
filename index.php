4dd4242fd20bd1172979d51cd86d76431eb25dba
gintaras68 <gintaras5783@yahoo.com>
2024-01-17 11:34:28 +0200

    Grįžtu prie apšilimo užduočių

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
       <h<?= $i ?>> <?= $i ?> </h<?= $i ?>>
   <?php } ?> -->
 
+  <!-- 1. Sukurkite 4 kintamuosius, kurie saugotų jūsų vardą, pavardę, gimimo metus ir šiuos metus (nebūtinai tikrus). Parašykite kodą, kuris pagal gimimo metus paskaičiuotų jūsų amžių ir naudodamas vardo ir pavardės kintamuosius atspausdintų tokį sakinį :
+"Aš esu Vardenis Pavardenis. Man yra XX metai(ų)". -->
   <?php
     $name = "Gintaras";
     $surname = "Misevičius";
